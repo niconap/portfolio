0d21eb993d4384135603a595a536cc9fecb4d0ba
Nico Nap <nico.nap@outlook.com>
2021-10-20 17:48:20 +0200

    Fix schema issue

--- backend/models/article.js
+++ backend/models/article.js
@@ -1,11 +1,12 @@
-var mongoose = 'mongoose';
-const { Schema } = mongoose;
+var mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const ArticleSchema = new Schema({
   title: String,
   content: [{ body: String }],
   date: { type: Date, default: Date.now },
   public: Boolean,
+  Author: { type: Schema.Types.ObjectId, ref: 'User' },
 });
 
 module.exports = mongoose.model('Article', ArticleSchema);
