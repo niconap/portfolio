d974bc566c31ba4b149cb73497c65f6efc0f8652
Nico Nap <nico.nap@outlook.com>
2021-10-20 17:48:09 +0200

    Create login system

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,59 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var dotenv = require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require('dotenv').config();
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
+const session = require('express-session');
+const passportJWT = require('passport-jwt');
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/user');
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: 'username', passwordField: 'password' },
+    function (username, password, done) {
+      User.findOne({ username: username }, (err, user) => {
+        if (err) return done(err);
+        if (!user) return done(null, false, { message: 'Incorrect username' });
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) return done(null, user);
+          return done(null, false, { message: 'Incorrect password' });
+        });
+      });
+    }
+  )
+);
+
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.PASSPORT_SECRET,
+    },
+    function (jwtPayload, callback) {
+      return user
+        .findOneById(jwtPayload.id)
+        .then((user) => {
+          return callback(null, user);
+        })
+        .catch((err) => {
+          return callback(err);
+        });
+    }
+  )
+);
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
 var mongoose = require('mongoose');
+const user = require('./models/user');
 var mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
@@ -24,6 +68,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: process.env.PASSPORT_SECRET }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 
@@ -37,7 +84,7 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  console.log(err);
   // render the error page
   res.status(err.status || 500);
   res.json({ message: 'An error occured' });
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,92 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const async = require('async');
+const bcrypt = require('bcryptjs');
+const { body, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.json({ message: 'Homepage' });
 });
+
+router.post('/auth/login', (req, res, next) => {
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    if (err || !user) {
+      return res.status(400).json({
+        message: 'Something went wrong',
+        user,
+        info,
+      });
+    }
+
+    req.login(user, { session: false }, (err) => {
+      if (err) res.json({ error: err });
+
+      const token = jwt.sign(user.toJSON(), process.env.PASSPORT_SECRET);
+      return res.json({ user, token });
+    });
+  })(req, res);
+});
+
+router.post('/auth/register', [
+  (res, req, next) => {
+    next();
+  },
+
+  body('firstname', 'First name must be longer than 3 characters.')
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body('lastname', 'Last name must be longer than 3 characters.')
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body('username', 'Username must be longer than 3 characters.')
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body('username', 'Username is already in use.').custom((value, { req }) => {
+    return new Promise((resolve, reject) => {
+      User.findOne({ username: req.body.username }, function (err, user) {
+        if (err) return next(err);
+        if (user && user.username == value) {
+          reject(new Error('Username is already in use.'));
+        }
+        resolve(true);
+      });
+    });
+  }),
+  body('password', 'Password must be longer than 3 characters.')
+    .trim()
+    .isLength({ min: 8 })
+    .escape(),
+  body('code', 'Code is incorrect.').trim().equals(process.env.CODE),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json({ errors: errors.array() });
+      return;
+    }
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+      if (err) return next(err);
+      var user = new User({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        username: req.body.username,
+        password: hashedPassword,
+      }).save((err, newUser) => {
+        if (err) return next(err);
+        res.json({
+          message: 'Signup complete!',
+          user: newUser.username,
+        });
+      });
+    });
+  },
+]);
 
 module.exports = router;
