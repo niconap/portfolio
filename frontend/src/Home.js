99a7b749d5ec43de48524858e008948e4eafe51b
Nico Nap <nico.nap@outlook.com>
2021-10-22 17:23:26 +0200

    Add basic homepage

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
 export default function Home() {
-  return <p>Home</p>;
+  return (
+    <div id="wrapper">
+      <section id="landing">
+        <h1>Nico Nap</h1>
+        <p>Hello! Welcome to my portfolio. Enjoy your stay.</p>
+        <div id="socials"></div>
+      </section>
+      <section id="about">
+        <h2>About me</h2>
+        <p>
+          Hi! Thank you so much for checking out my portfolio! My name is Nico,
+          I'm a student from the Netherlands with a big passion for coding and
+          web design. I started this portfolio to show off my work and to keep
+          myself motivated to continue coding. Currently I'm working on both
+          front-end (mostly JavaScript) and back-end (mostly Node.js)
+          development.
+        </p>
+      </section>
+    </div>
+  );
 }
